bc40b88ada637c47f6e0d991584bbcbd1bb8f6c1
Yash Dixit <yashdixit0885@gmail.com>
2022-09-06 14:27:20 -0600

    added further steps for app intall

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #My Apps
+    'learning_logs',
+
+    #Default Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ learning_logs/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Topic(models.Model):
+    """A topic that user is learning about"""
+
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
