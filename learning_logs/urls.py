3b43b06a7b55cd7753cf30d38ae1bd7839e57195
Yash Dixit <yashdixit0885@gmail.com>
2022-09-07 18:05:50 -0600

    added topics model, view and template code.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,8 @@ app_name = 'learning_logs'
 
 urlpatterns = [
     # Home Page
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+
+    # Page that shows all topics
+    path('topics/',views.topics,name='topics')
 ]
