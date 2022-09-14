5514d15b7dd3b7e65a132f5121f3121ee7675ba3
Yash Dixit <yashdixit0885@gmail.com>
2022-09-14 11:44:12 -0600

    added registration url,view function and template. Project is running at this commit!

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 """Defines URL patterns for users"""
 
 from django.urls import path,include
+from .import views
 
 
 app_name = 'users'
@@ -8,4 +9,7 @@ app_name = 'users'
 urlpatterns = [
     # Include default auth urls.
     path('',include('django.contrib.auth.urls')),
+
+    #Registration Page
+    path('register/', views.register,name='register'),
 ]
